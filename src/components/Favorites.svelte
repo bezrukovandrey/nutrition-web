e802ae6e4936a6718684b7d2821c2576c7edc585
bezrukovandrey <80787549+bezrukovandrey@users.noreply.github.com>
2024-06-17 21:30:01 +0300

    Update Favorites.svelte

--- src/components/Favorites.svelte
+++ src/components/Favorites.svelte
@@ -12,13 +12,13 @@
   let searchTerm = '';
   
 
-  // Данные поисковой выдачи
-  // Замените [...] на ваш массив данных
-  const pageSize = 3; // Количество элементов на странице
+  
+  
+  const pageSize = 3; 
   let currentPageMeals = 0;
-  let currentPageProducts = 0; // Текущая страница
+  let currentPageProducts = 0; 
 
-  // Вычисляемое свойство для разделения результатов на страницы
+
   $: paginatedFavoriteMeals = favoriteMealsData
   .filter(meal => meal.title.toLowerCase().includes(searchTerm.toLowerCase()))
   .slice(currentPageMeals * pageSize, (currentPageMeals + 1) * pageSize);
@@ -27,14 +27,14 @@ $: paginatedFavoriteProducts = favoriteProductsData
   .filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
   .slice(currentPageProducts * pageSize, (currentPageProducts + 1) * pageSize);
 
-  // Обработчик для переключения страниц вперед
+  
   function nextPageMeals() {
     if ((currentPageMeals + 1) * pageSize < favoriteMealsData.length) {
       currentPageMeals++;
     }
   }
 
-  // Обработчик для переключения страниц назад
+  
   function prevPageMeals() {
     if (currentPageMeals > 0) {
       currentPageMeals--;
@@ -47,20 +47,20 @@ $: paginatedFavoriteProducts = favoriteProductsData
     }
   }
 
-  // Обработчик для переключения страниц назад
+  
   function prevPageProducts() {
     if (currentPageProducts > 0) {
       currentPageProducts--;
     }
   }
 
-  // Создаем массив для точек внизу компонента
+ 
   $: totalPagesMeals = Math.ceil(favoriteMealsData.length / pageSize);
   $: pagesMeals = Array.from({ length: totalPagesMeals }, (_, i) => i);
   $: totalPagesProducts = Math.ceil(favoriteProductsData.length / pageSize);
   $: pagesProducts = Array.from({ length: totalPagesProducts }, (_, i) => i);
 
-  // Обработчик для переключения на конкретную страницу
+  
   function goToPageMeals(index) {
     currentPageMeals = index;
   }
@@ -70,45 +70,37 @@ $: paginatedFavoriteProducts = favoriteProductsData
   }
 
   function search() {
-  // Выполните любую дополнительную логику, которая может понадобиться перед фильтрацией
   const searchInput = document.getElementById('searchInput');
   if (searchInput) {
     searchTerm = searchInput.value.toLowerCase().trim();
   }
-  // Обновите результаты поиска
   currentPageMeals = 0;
   currentPageProducts = 0;
 }
 
   const removeFromFavoriteMeals = (id) => {
-  // Удаляем блюдо из глобального хранилища избранных блюд
   favoriteMeals.update((meals) => {
     return meals.filter(meal => meal.id !== id);
   });
 
-  // Добавляем уведомление об успешном удалении
   toast.push('Meal is removed from favorites!', {
     theme: { '--toastBackground': '#53A5AE', '--toastColor': '#FFF' }
   });
 };
 
 const removeFromFavoriteProducts = (id) => {
-  // Удаляем блюдо из глобального хранилища избранных блюд
   favoriteProducts.update((products) => {
     return products.filter(product => product.id !== id);
   });
-
-  // Добавляем уведомление об успешном удалении
   toast.push('Product is removed from favorites!', {
     theme: { '--toastBackground': '#53A5AE', '--toastColor': '#FFF' }
   });
 };
 
-  // Навешиваем слушатели событий при монтировании компонента
+  
   onMount(() => {
     console.log("favorite meals", favoriteMealsData);
     console.log("favorite products", favoriteProductsData);
-    // Добавляем слушатель событий для управления стрелками
     function handleArrowKeysMeals(event) {
       if (event.key === "ArrowLeft") {
         prevPageMeals();
@@ -127,7 +119,7 @@ const removeFromFavoriteProducts = (id) => {
     window.addEventListener("keydown", handleArrowKeysMeals);
     window.addEventListener("keydown", handleArrowKeysProducts);
 
-    // Удаляем слушатель событий при размонтировании компонента
+    
     return () => {
       window.removeEventListener("keydown", handleArrowKeysMeals);
       window.removeEventListener("keydown", handleArrowKeysProducts);
@@ -192,7 +184,7 @@ const removeFromFavoriteProducts = (id) => {
     </div>
   {/if}
   {#if paginatedFavoriteMeals.length > 0}
-    <!-- Контейнер для карточек результатов поиска -->
+    
     <div
       class="shrink-0 mt-10 h-px bg-black border border-black border-solid w-full"
     ></div>
@@ -202,10 +194,10 @@ const removeFromFavoriteProducts = (id) => {
       Meals
     </h1>
     <article class="my-10 flex flex-wrap gap-6 justify-center">
-      <!-- Отображение карточек результатов поиска -->
+      
       {#each paginatedFavoriteMeals as favorite}
         <figure class="flex flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <!-- Отображение изображения и названия результата -->
+    
           <img
             src={favorite.image}
             alt={favorite.title}
@@ -215,7 +207,7 @@ const removeFromFavoriteProducts = (id) => {
           <div class="mt-6 text-sm sm:text-m font-semibold text-center">
             {favorite.title}
           </div>
-          <!-- Кнопка "Details" -->
+          
           <div class="flex flex-row justify-between">
           <button
             class="self-center px-6 py-3 text-sm sm:text-m bg-darkGreen hover:bg-mainGreen border border-darkGreen border-solid text-white cursor-pointer mt-4"
@@ -234,11 +226,11 @@ const removeFromFavoriteProducts = (id) => {
       {/each}
     </article>
 
-    <!-- Контейнер для точек и стрелок навигации -->
+    
     <div
       class="flex items-center justify-between w-full px-12 bg-mainBeige mx-auto gap-8 flex-wrap"
     >
-      <!-- Точки для пагинации -->
+      
       <nav class="flex gap-2 px-6 my-auto">
         {#each pagesMeals as page}
           <button
@@ -250,9 +242,9 @@ const removeFromFavoriteProducts = (id) => {
         {/each}
       </nav>
 
-      <!-- Кнопки для переключения страниц -->
+      
       <div class="flex gap-4">
-        <!-- Кнопка "Previous" -->
+       
         <button
           class="flex justify-center items-center p-2 border border-black border-solid rounded-[50px] bg-darkGreen hover:bg-mainGreen"
           tabindex="0"
@@ -266,7 +258,7 @@ const removeFromFavoriteProducts = (id) => {
           />
         </button>
 
-        <!-- Кнопка "Next" -->
+    
         <button
           class="flex justify-center items-center p-2 border border-black border-solid rounded-[50px] bg-darkGreen hover:bg-mainGreen"
           tabindex="0"
@@ -284,7 +276,7 @@ const removeFromFavoriteProducts = (id) => {
     </div>
   {/if}
   {#if paginatedFavoriteProducts.length > 0}
-    <!-- Контейнер для карточек результатов поиска -->
+    
     <div
       class="shrink-0 mt-10 h-px bg-black border border-black border-solid w-full"
     ></div>
@@ -294,10 +286,10 @@ const removeFromFavoriteProducts = (id) => {
       Products
     </h1>
     <article class="my-10 flex flex-wrap gap-6 justify-center">
-      <!-- Отображение карточек результатов поиска -->
+     
       {#each paginatedFavoriteProducts as favorite}
         <figure class="flex flex-col w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
-          <!-- Отображение изображения и названия результата -->
+         
           <img
             src="https://img.spoonacular.com/ingredients_500x500/{favorite.image}"
             alt={favorite.title}
@@ -309,7 +301,7 @@ const removeFromFavoriteProducts = (id) => {
           >
             {favorite.name}
         </div>
-          <!-- Кнопка "Details" -->
+         
           <div class="flex flex-row justify-between">
             <button
               class="self-center px-6 py-3 text-sm sm:text-m bg-darkGreen hover:bg-mainGreen border border-darkGreen border-solid text-white cursor-pointer mt-4"
@@ -328,11 +320,11 @@ const removeFromFavoriteProducts = (id) => {
       {/each}
     </article>
 
-    <!-- Контейнер для точек и стрелок навигации -->
+    
     <div
       class="flex items-center justify-between w-full px-12 bg-mainBeige mx-auto gap-8 flex-wrap"
     >
-      <!-- Точки для пагинации -->
+     
       <nav class="flex gap-2 px-6 my-auto">
         {#each pagesProducts as page}
           <button
@@ -344,9 +336,9 @@ const removeFromFavoriteProducts = (id) => {
         {/each}
       </nav>
 
-      <!-- Кнопки для переключения страниц -->
+      
       <div class="flex gap-4">
-        <!-- Кнопка "Previous" -->
+      
         <button
           class="flex justify-center items-center p-2 border border-black border-solid rounded-[50px] bg-darkGreen hover:bg-mainGreen"
           tabindex="0"
@@ -360,7 +352,7 @@ const removeFromFavoriteProducts = (id) => {
           />
         </button>
 
-        <!-- Кнопка "Next" -->
+      
         <button
           class="flex justify-center items-center p-2 border border-black border-solid rounded-[50px] bg-darkGreen hover:bg-mainGreen"
           tabindex="0"
